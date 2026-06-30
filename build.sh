19520513b9b4f8d915274ab4e97cae908e8ecf7b
Quadratech188 <quadratech188@gmail.com>
2026-06-30 15:28:32 +0900

    chore: Bump to 3.2.0

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mock -r fedora-43-x86_64 --no-clean --buildsrpm --spec kime.spec --sources .
-cp /var/lib/mock/fedora-43-x86_64/result/*.src.rpm .
-
-mock -r fedora-43-x86_64 --no-clean *.src.rpm --enable-network
